e7f7bebc72eeba1fb6b6a507c083744e93b683e2
Victorzhiweii <41710398+Victorzhiweii@users.noreply.github.com>
2019-03-08 17:01:13 +0800

    Create sock.h

---
+++ sock.h
@@ -0,0 +1,16 @@
+#ifndef __TCPSOCKET_H__
+#define __TCPSOCKET_H__
+
+
+
+// #define BACKLOG 	10 
+// #define LENGTH 512              		// Buffer length 
+
+void tcpSocketThread(void * arg);
+
+
+
+
+
+#endif
+
